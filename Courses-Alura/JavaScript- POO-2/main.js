62a0cc8b77ef47871a75b66e83a9176b4c6b8102
Andrew Gomes <andrewgomes1328@gmail.com>
2023-05-16 20:22:41 -0300

    up

---
+++ Courses-Alura/JavaScript- POO-2/main.js
@@ -0,0 +1,12 @@
+let listaDeItens = []
+
+const listBuy = {
+    item1: 'Fogão',
+    item2: 'Geladeira',
+    item3: 'Televisão',
+    total: function () {
+        console.log(listBuy);
+    }
+}
+
+listBuy.total();
