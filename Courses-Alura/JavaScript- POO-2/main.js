0f1b6ba7de0066c722dbfa1a205a939f3161f9af
Andrew Gomes <andrewgomes1328@gmail.com>
2023-05-18 20:26:52 -0300

    up1

--- Courses-Alura/JavaScript- POO-2/main.js
+++ Courses-Alura/JavaScript- POO-2/main.js
@@ -2,10 +2,12 @@ let listaDeItens = []
 
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
+const ulitens = document.getElementById('lista-de-itens')
 
 form.addEventListener("submit", function (evento) {
     evento.preventDefault()
     salvarItens()
+    mostrarItem()
 })
 
 function salvarItens() {
@@ -23,4 +25,27 @@ function salvarItens() {
     console.log(listaDeItens)
 }
 
+function mostrarItem() {
+    ulitens.innerHTML = ''
+    listaDeItens.forEach((elemento,index)=> {
+        ulitens.innerHTML += `
+    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+    `
+    })
 
+    const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
+
+    inputsCheck.forEach(i => {
+        i.addEventListener('click', (evento)=> {
+            console.log(evento.target.parentElement.parentElement.getAttribute('data-value'))
+        })
+    })
+}
