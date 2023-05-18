ca78d19a17382d4bfb36b10b5b2ea98d60663005
Andrew Gomes <andrewgomes1328@gmail.com>
2023-05-17 21:40:46 -0300

    atualização do dia (17/06/2023) !
    Nesse código adicionei condições e funcionalidades, como por exemplo: Colocar o item digitado dentro de um objeto! Crie funções para isso.

--- Courses-Alura/JavaScript- POO-2/main.js
+++ Courses-Alura/JavaScript- POO-2/main.js
@@ -1,1 +1,26 @@
 let listaDeItens = []
+
+const form = document.getElementById("form-itens")
+const itensInput = document.getElementById("receber-item")
+
+form.addEventListener("submit", function (evento) {
+    evento.preventDefault()
+    salvarItens()
+})
+
+function salvarItens() {
+    const itemCompras = itensInput.value
+    const checarDuplicado = listaDeItens.some((elemento) => elemento.valor.toUpperCase() === itemCompras.toUpperCase())
+
+    if(checarDuplicado) {
+        alert("Item já foi adicionado!")
+    } else {
+        listaDeItens.push ({
+            valor: itemCompras
+        })
+    }
+
+    console.log(listaDeItens)
+}
+
+
