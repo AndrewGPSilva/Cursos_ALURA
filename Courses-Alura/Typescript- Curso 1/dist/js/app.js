40951ee493abdcc202e31201f89651482eb0a9a2
Andrew Gomes <andrewgomes1328@gmail.com>
2023-06-17 20:27:06 -0300

    update

--- Courses-Alura/Typescript- Curso 1/dist/js/app.js
+++ Courses-Alura/Typescript- Curso 1/dist/js/app.js
@@ -1,6 +1,7 @@
-import { Negociacao } from './models/negociacao.js';
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.data);
-console.log(negociacao.quantidade);
-console.log(negociacao.valor);
-console.log(negociacao.volume);
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const formulario = document.querySelector(".form");
+formulario.addEventListener("submit", event => {
+    event.preventDefault();
+    controller.adicionar();
+});
