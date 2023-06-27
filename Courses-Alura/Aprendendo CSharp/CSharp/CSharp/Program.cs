520e64b75348a15fb164c3935d486b7162287a07
Andrew Gomes <andrewgomes1328@gmail.com>
2023-06-27 20:09:00 -0300

    up

--- Courses-Alura/Aprendendo CSharp/CSharp/CSharp/Program.cs
+++ Courses-Alura/Aprendendo CSharp/CSharp/CSharp/Program.cs
@@ -1,7 +1,15 @@
 ﻿class Musica
 {
     public string nome;
-    public DateTime dataLancamento;
     public int duracao;
     public bool disponivel;
+
+    public Musica(string nome, int duracao, bool disponivel)
+    {
+        this.nome = nome;
+        this.duracao = duracao;
+        this.disponivel = disponivel;
+    }
+
+    Musica musica1 = new Musica("Andrew", 300, true);
 }
