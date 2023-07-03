3c42497803b4f205186d75404f3d63dc419e9565
Andrew Gomes <andrewgomes1328@gmail.com>
2023-07-02 21:10:01 -0300

    up

--- Courses-Alura/Login e Senha com PHP e MYSQL/config.php
+++ Courses-Alura/Login e Senha com PHP e MYSQL/config.php
@@ -1,7 +1,7 @@
 <?php 
     define('HOST', 'localhost');
     define('USER', 'root');
-    define('PASS', 'gongon2806**');
+    define('PASS', '*******');
     define('BASE', 'sislogin');
 
     $conn = new mysqli(HOST, USER, PASS, BASE);
