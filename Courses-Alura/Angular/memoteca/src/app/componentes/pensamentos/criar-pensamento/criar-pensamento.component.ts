b97450d15a1c35ba4ab6f39264e30f07ff579b1d
Andrew Gomes <andrewgomes1328@gmail.com>
2023-07-21 23:49:48 -0300

    Usando Two way Data binding

--- Courses-Alura/Angular/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ Courses-Alura/Angular/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -11,7 +11,7 @@ export class CriarPensamentoComponent implements OnInit {
     id: '1',
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
-    modelo: ''
+    modelo: 'modelo1'
   }
 
   constructor() {}
@@ -22,7 +22,7 @@ export class CriarPensamentoComponent implements OnInit {
     alert("UHuuuuuuuuuuuu")
   }
 
-  cancelarPensamento() {
+  cancelar() {
     alert("Cancelado!")
   }
 }
