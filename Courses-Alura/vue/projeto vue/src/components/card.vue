8bde75045fc1700268668f1dae444d9313fd6270
Andrew Gomes <andrewgomes1328@gmail.com>
2023-08-28 20:44:30 -0300

    utilizando a arvore de componentização

--- Courses-Alura/vue/projeto vue/src/components/card.vue
+++ Courses-Alura/vue/projeto vue/src/components/card.vue
@@ -1,22 +1,22 @@
 <template>
-    <div>
-      <div class="card" style="width: 18rem;">
-    <img src="/src/assets/onepiece.jpg" class="card-img-top" alt="Foto de One Piece">
-    <div class="card-body">
-      <h5 class="card-title">One Piece</h5>
-      <p class="card-text">Essa é a famosa tripulação do Yonkou Luffy do Chapéu de Palha</p>
-      <a href="#" class="btn btn-primary">Acessar sinopse</a>
+  <div>
+    <div class="card" style="width: 18rem;">
+      <img src="/src/assets/onepiece.jpg" class="card-img-top" alt="Foto de One Piece">
+      <div class="card-body">
+        <h5 class="card-title">One Piece</h5>
+        <p class="card-text">Essa é a famosa tripulação do Yonkou Luffy do Chapéu de Palha</p>
+        <a href="#" class="btn btn-primary">Acessar sinopse</a>
+      </div>
     </div>
   </div>
-    </div>
 </template>
   
 <script>
-  export default {
-    name: 'card',
-    data() {
-      return {}
-    }
+export default {
+  name: 'card',
+  data() {
+    return {}
   }
+}
 </script>
   
